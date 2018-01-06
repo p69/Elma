c41805f930faa09e99c3d1de0be9eefba3ec7a51
pavel-shilyagov <shilyagov.p@gmail.com>
2018-01-06 19:03:28 +0300

    Fix build
    Upgrade kotlin, enable D8, update build tools

--- app/src/main/java/com/example/pavelshilyagov/tryelmish/LithoApplication.kt
+++ app/src/main/java/com/example/pavelshilyagov/tryelmish/LithoApplication.kt
@@ -1,6 +1,8 @@
 package com.example.pavelshilyagov.tryelmish
 
 import android.app.Application
+import android.content.Context
+import android.support.multidex.MultiDex
 import com.facebook.soloader.SoLoader
 
 
@@ -8,5 +10,10 @@ class LithoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         SoLoader.init(this,false)
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 }
